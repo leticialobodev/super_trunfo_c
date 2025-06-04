9482a611efd4fdc9f88019a8b9fcf766206d45bb
Leticia de Cassia Maia Lobo <leticiadecassiamaialobo@gmail.com>
2025-06-03 22:20:20 -0300

    feat: adiciona variáveis básicas para estudo de C

---
+++ nivel_novato.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int idade; 
+    int quantidade = 1;
+    float altura = 1.60;
+    double peso = 65.2;
+    char letra = 'A'; 
+    char nome[20] = "leticia";
+    
+    idade = 25;
+
+    quantidade = 10;
+
+
+    
+
+return 0;
+}
