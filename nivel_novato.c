8a55eb53ede671b65ff4c6b548f5f02d573e2066
Leticia de Cassia Maia Lobo <leticiadecassiamaialobo@gmail.com>
2025-06-11 19:35:39 -0300

    feat: Conclusao do Nivel Novato do Super Trunfo, referente ao Cadastro de Duas Cartas

--- nivel_novato.c
+++ nivel_novato.c
@@ -1,64 +1,124 @@
-#include <stdio.h> //inclui a biblioteca que vamos usar
+#include <stdio.h> //inclui a biblioteca principal
 #include <string.h> //inclui uma biblioteca com funções para manipular strings, como strcspn.
 
-int main() { //inclui a função prinicpal dentro da qual vamos fazer tudo acontecer feito mágica!
+int main() { //inclui a função principal que vamos utilizar.
 
 
   // ==============================================================================
   // DECLARAÇÃO DE VARIÁVEIS - CARTA 01 e CARTA 02
   // ==============================================================================
 
-  char estado1; //define um caractere de A a H pra identificar o Estado da cidade 01
-  char codigo1[4]; //define o código da carta usando a letra escolhida e um número de 1 a 4
-  char nome_cidade1[50]; //define o nome da cidade, que o usuário pode colocr em até 50 caracteres
-  int populacao1; //define o número de seres viventes nessa cidade
-  float area1; //define a área da cidade
-  float pib1; //define o PIB dessa cidade
-  int pontos_turisticos1; //define quantos pontos turísticos ela tem
-  char temp_buffer[100]; //Buffer temporário para limpar o '\n' que sobra nas leituras.
+  char estado1; //Armazena um caractere de A a H pra identificar o Estado da cidade 01
+  char codigo1[4]; //Armazena o código da carta usando a letra escolhida e um número de 1 a 4
+  char nome_cidade1[50]; //Armazena o nome da cidade, que o usuário pode colocar em até 50 caracteres
+  int populacao1; //Armazena a população dessa cidade
+  float area1; //Armazena a área da cidade
+  float pib1; //Armazena o PIB dessa cidade
+  int pontos_turisticos1; //Armazena quantos pontos turísticos ela tem
+  char temp_buffer[100]; //Buffer temporário para limpar o '\n' restante das leituras.
+
+  char estado2; //Armazena um caractere de A a H pra identificar o Estado da cidade 02
+  char codigo2[4]; //Armazena o código da carta usando a letra escolhida e um número de 1 a 4
+  char nome_cidade2[50]; //Armazena o nome da cidade, que o usuário pode colocar em até 50 caracteres
+  int populacao2; //Armazena a população dessa cidade
+  float area2; //Armazena a área dessa cidade
+  float pib2; //Armazena o PIB dessa cidade
+  int pontos_turisticos2; //Armazena quantos pontos turísticos ela tem
 
   // ===============================================================================
-  // CABEÇALHO DO JOGO E CARTAS 01 E 02
+  // CABEÇALHO DO JOGO, ENTRADA E EXIBIÇÃO DE DADOS DAS CARTAS 01 E 02
   // ===============================================================================
 
-  printf("\n-------- Jogo de Cartas Super Trunfo: Tema Paises! --------\n"); //só pra mostrar ao usuário do que estamos falando
-  printf("\n"); //um espacinho não faz mal a ninguém, não é mesmo? ;)
+  //Os blocos para as cartas 01 e 02 seguem ambos a mesma lógica de leitura/exibição!
+  //As perguntas e leituras são feitas sequencialmente.
 
-
-  printf("CARTA NUMERO 01\n"); //diz que essa é a carta número 1
-
+  printf("----- Ola! Bem-vindo ao Jogo Super Trunfo! -----\n"); //Saudação ao usuário
+  printf("\n-------- Jogo de Cartas Super Trunfo: Tema Paises! --------\n"); //Para introduzir ao usuário do que se trata o programa
+  printf("\n"); //Espaço para melhor entendimento e clareza no código
 
   // ===============================================================================
   // ENTRADA DE DADOS - CARTA 01
   // ===============================================================================
 
-  printf("Escolha uma letra de A a H para o Estado desta carta: \n"); //pede ao usuário que escolha uma letra para esse Estado
-  scanf(" %c", &estado1); //recebe o caractere que o usuário inserir e coloca na variável estado_01.
-  fgets(temp_buffer, 100, stdin);
+  printf("Agora, voce devera inserir os dados que pedimos abaixo para cadastrar duas cartas no jogo!\n"); //Interação com o usuário para maior usabilidade, melhor experiência e clareza.
+  printf("Fique a vontade para cadastrar os dados que quiser, sempre obedecendo as regras!\n");
+  printf("\n");
+  printf("Obs: Os dados inseridos não precisam ser verdadeiros ou baseados em cidades reais.\n");
 
-  printf("Escolha um codigo para a carta 01 (junte a letra que voce escolheu e um numero de 01 a 04): \n");  //dá instruções ao usuário pra que ele escreva o código da carta
-  fgets(codigo1, 4, stdin); //recebe o código que o usuário inserir
+  printf("\n");
+  printf("\n");
+
+  printf("CARTA NUMERO 01\n"); //Informa que essa é a carta número 1
+
+  printf("\n");
+
+  printf("Escolha uma letra de A a H para o Estado desta carta: \n"); //Pede ao usuário que escolha uma letra para esse Estado.
+  scanf("%c", &estado1); //Recebe o caractere que o usuário inserir e coloca na variável estado_01.
+  fgets(temp_buffer, sizeof(temp_buffer), stdin); //Limpa o buffer de entrada (stdin) após uma leitura feita com scanf.
+
+  printf("Escolha um codigo para a carta 01 (escolha um numero de 01 a 04, utilizando dois algarismos): \n");  //Instrui o usuário para que ele escolha e insira o código da carta
+  fgets(codigo1, sizeof(codigo1), stdin); //Recebe o código que o usuário inserir
   codigo1[strcspn(codigo1, "\n")] = '\0'; //Limpa o '\n' que o fgets adiciona ao final da string.
 
-  printf("Qual o nome da cidade 01?: \n"); //pergunta ao usuário qual o nome da cidade 1
-  fgets(nome_cidade1, 50, stdin); //recebe o nome da cidade inserido.
+  printf("Qual o nome da cidade 01?: \n"); //Pergunta ao usuário qual o nome da cidade 1
+  fgets(nome_cidade1, sizeof(nome_cidade1), stdin); //Recebe o nome da cidade inserido.
   nome_cidade1[strcspn(nome_cidade1, "\n")] = '\0'; //Limpa novamente o '\n' deixado pelo fgets
 
   printf("Qual a populacao dessa cidade?(informe um numero sem pontos ou virgulas): \n");
-  scanf("%d", &populacao1); //recebe a população inserida
-  fgets(temp_buffer, 100, stdin);
+  scanf("%d", &populacao1); //Recebe a população inserida
+  fgets(temp_buffer, sizeof(temp_buffer), stdin);
 
-  printf("Qual a area da cidade 01?(informe um numero decimal): \n"); //pergunta qual a área da cidade 01 ao usuário
-  scanf("%f", &area1); //recebe a área inserida
-  fgets(temp_buffer, 100, stdin);
+  printf("Qual a area da cidade 01?(informe um numero decimal, utilizando apenas um ponto para representar a virgula): \n"); //Pergunta qual a área da cidade 01 ao usuário
+  scanf("%f", &area1); //Recebe a área inserida
+  fgets(temp_buffer, sizeof(temp_buffer), stdin);
   
-  printf("Qual o PIB dessa cidade?(informe um numero decimal): \n"); //pergunta qual o PIB dessa cidade
-  scanf("%f", &pib1); //recebe o valor decimal inserido
-  fgets(temp_buffer, 100, stdin);
+  printf("Qual o PIB dessa cidade?(informe um numero decimal, também com ponto): \n"); //Pergunta qual o PIB dessa cidade
+  scanf("%f", &pib1); //Recebe o valor decimal inserido
+  fgets(temp_buffer, sizeof(temp_buffer), stdin);
 
-  printf("E quantos pontos turísticos ela tem?(informe um numero inteiro): \n"); //pergunta quantos pontos turísticos ela tem
-  scanf(" %d", &pontos_turisticos1); //recebe o valor inteiro inserido
-  fgets(temp_buffer, 100, stdin);
+  printf("E quantos pontos turísticos ela tem?(informe um numero inteiro): \n"); //Pergunta quantos pontos turísticos ela tem
+  scanf("%d", &pontos_turisticos1); //Recebe o valor inteiro inserido
+  fgets(temp_buffer, sizeof(temp_buffer), stdin);
+
+  // ========================================================
+  // ENTRADA DE DADOS CARTA 02
+  // ========================================================
+
+  printf("\n");
+  printf("\n");
+
+  printf("CARTA NUMERO 02\n");
+
+  printf("\n");
+
+  printf("Escolha uma letra de A a H para o Estado desta carta(Pode repetir a letra se o estado for o mesmo da carta 01): \n");
+  scanf("%c", &estado2);
+  fgets(temp_buffer, sizeof(temp_buffer), stdin);
+
+  printf("Escolha um codigo para a carta 02 (escolha um numero de 01 a 04, utilizando dois algarismos): \n");
+  fgets(codigo2, sizeof(codigo2), stdin);
+  codigo2[strcspn(codigo2, "\n")] = '\0';
+
+  printf("Qual o nome da cidade 02?: \n");
+  fgets(nome_cidade2, sizeof(nome_cidade2), stdin);
+  nome_cidade2[strcspn(nome_cidade2, "\n")] = '\0';
+
+  printf("Qual a populacao dessa cidade? (informe um numero sem pontos ou virgulas): \n");
+  scanf("%d", &populacao2);
+  fgets(temp_buffer, sizeof(temp_buffer), stdin);
+
+  printf("Qual a area da cidade 02? (Informe um número decimal, utilizando o ponto): \n");
+  scanf("%f", &area2);
+  fgets(temp_buffer, sizeof(temp_buffer), stdin);
+
+  printf("Qual o PIB dessa cidade? (Informe um numero decimal, utilizando o ponto tambem): \n");
+  scanf("%f", &pib2);
+  fgets(temp_buffer, sizeof(temp_buffer), stdin);
+
+  printf("E quantos pontos turisticos ela tem? (Informe um numero inteiro): \n");
+  scanf("%d", &pontos_turisticos2);
+  fgets(temp_buffer, sizeof(temp_buffer), stdin);
+
 
   // ========================================================
   // EXIBIÇÃO DOS DADOS DA CARTA 01
@@ -70,13 +130,40 @@ int main() { //inclui a função prinicpal dentro da qual vamos fazer tudo acont
     printf("╚════════════════════════════════════════╝\n");
 
     printf("Estado: %c\n", estado1);
-    printf("Codigo: %s\n", codigo1);
+    printf("Codigo: %c%s\n", estado1, codigo1);
     printf("Nome da Cidade: %s\n", nome_cidade1);
     printf("Populacao: %d\n", populacao1);
     printf("Area: %.2f km²\n", area1);
     printf("PIB: %.2f milhoes de reais\n", pib1);
     printf("Pontos Turisticos: %d\n", pontos_turisticos1);
     
+
+
+    // =====================================================
+    // EXIBIÇÃO DOS DADOS DA CARTA 02
+    // =====================================================
+
+    printf("Seguem abaixo os dados que você inseriu na carta 02!\n");
+
+    printf("\n╔════════════════════════════════════════╗\n");
+      printf("║            Dados da Carta 02           ║\n");
+      printf("╚════════════════════════════════════════╝\n");
+  
+  printf("Estado: %c\n", estado2);
+  printf("Codigo: %c%s\n", estado2, codigo2);
+  printf("Nome da Cidade: %s\n", nome_cidade2);
+  printf("Populacao: %d\n", populacao2);
+  printf("Area: %.2f km²\n", area2);
+  printf("PIB: %.2f milhoes de reais\n", pib2);
+  printf("Pontos Turisticos: %d\n", pontos_turisticos2);
+
+  //=================================================================
+  // FINALIZAÇÃO
+  //=================================================================
+
+  printf("\n");
+  printf("Parabens, voce cadastrou as cartas corretamente!\n");
+  printf("Obrigada por participar!");
 
 return 0;
 }
